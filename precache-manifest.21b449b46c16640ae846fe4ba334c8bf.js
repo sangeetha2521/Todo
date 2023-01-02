d190a1d87232d70a9aaab60bddc9c0af3060b636
sangeetha2521 <sangeens8101@gmail.com>
2023-01-02 18:20:18 +0530

    Updates

---
+++ precache-manifest.21b449b46c16640ae846fe4ba334c8bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "99d18f1a4baf09294f74",
+    "url": "/Todo/static/css/main.8c0b914c.chunk.css"
+  },
+  {
+    "revision": "99d18f1a4baf09294f74",
+    "url": "/Todo/static/js/main.5aa726d0.chunk.js"
+  },
+  {
+    "revision": "6fab73d75525f9d6064a",
+    "url": "/Todo/static/js/runtime~main.ee49a031.js"
+  },
+  {
+    "revision": "6bd81741109c7d4a538a",
+    "url": "/Todo/static/js/2.f84590e8.chunk.js"
+  },
+  {
+    "revision": "1ed9bb8d9dd9c4f9a730004e70b60628",
+    "url": "/Todo/index.html"
+  }
+];
